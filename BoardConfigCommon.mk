06f202d0ea4aca574b28e73d9889d3c798cf80c9
Unicote <arvogl874@gmail.com>
2021-07-01 22:05:22 +0300

    sm6150-common: Set loop.max_part to 16
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,7 +96,7 @@ BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
-BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += loop.max_part=16
 
 # TARGET_KERNEL_APPEND_DTB handling
 ifeq ($(strip $(PRODUCT_USE_DYNAMIC_PARTITIONS)),true)
