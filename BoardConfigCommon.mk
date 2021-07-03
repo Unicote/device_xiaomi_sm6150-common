57c946f87de8a83a90ab19df8b73c79ddfaa8f98
Unicote <arvogl874@gmail.com>
2021-07-03 09:13:08 +0300

    sm6150-common: Import system_ext.prop from surya
    Signed-off-by: Unicote <arvogl874@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,6 +129,7 @@ TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # QCOM
